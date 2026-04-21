91c12332c1df30c4acae86c422289912a478d5fe
JavierTLoina <tejedajavier063@gmail.com>
2026-04-21 14:11:00 -0400

    Fixing layout

--- NotFar/src/App.tsx
+++ NotFar/src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import "./App.scss";
+import "./App.css";
 import MapView from "./components/MapView";
 import { grafo } from "./data/grafo";
 import { tecnicos } from "./data/tecnico";
